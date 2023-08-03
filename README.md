beda3fbf1cb0aa24830857128fff21dbc2572019
soroshsabz <soorosh_abi@hotmail.com>
2023-08-03 19:36:05 +0330

    Merge Feature/1-add-base-code (#1)

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # IpTables.Api
 API for local iptables management
+
+This project powered on [IPTables.Net](https://github.com/splitice/IPTables.Net) for interacting with Linux IPTables
---
+++ Test/UnitTests/BSN.IpTables.Api.Tests/Controllers/HomeControolerTest.cs
@@ -0,0 +1,32 @@
+﻿using BSN.IpTables.Api.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IpTables.Api.Tests.Controllers
+{
+    internal class HomeControolerTest
+    {
+        [SetUp]
+        public void Initialize()
+        {
+            controller = new HomeController();
+        }
+
+        [Test]
+        public void IndexNormalCalling_ShloudReturnsOk()
+        {
+            IActionResult result = controller.Index(); 
+            Assert.That(result, Is.Not.Null);
+            Assert.That(result, Is.TypeOf(typeof(OkResult)));
+        }
+
+        [TearDown] public void TearDown() { }
+
+        private HomeController controller;
+    }
+}
