10614a8d845897aec5f5a63b774ef04b7d0f85db
soroshsabz <soorosh_abi@hotmail.com>
2023-08-03 18:38:47 +0330

    Add some files for remote testing

---
+++ Source/BSN.IpTables.Api/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BSN.IpTables.Api.Controllers
+{
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet]
+        [Route("/")]
+        public IActionResult Index()
+        {
+            return Ok();
+        }
+    }
+}
