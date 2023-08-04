6e7f332a7702b737f5aa3e44121c3856ff5a1879
soroshsabz <soorosh_abi@hotmail.com>
2023-08-04 19:45:23 +0330

    Use new Startup model

--- Source/BSN.IpTables.Api/Program.cs
+++ Source/BSN.IpTables.Api/Program.cs
@@ -10,52 +10,19 @@ namespace BSN.IpTables.Api
     {
         public static void Main(string[] args)
         {
-            var configurationBuilder = new ConfigurationBuilder();
-            configurationBuilder.AddJsonFile("autofac.json");
-            var autoFacConfigurationModule = new ConfigurationModule(configurationBuilder.Build());
-            var temp = new IpTablesDotNetSystem();
-
             var builder = WebApplication.CreateBuilder(args);
 
+            var startup = new Startup(builder.Configuration);
+            startup.ConfigureServices(builder.Services);
+
             builder.Host.UseServiceProviderFactory(new AutofacServiceProviderFactory());
-            builder.Host.ConfigureContainer<ContainerBuilder>((containerBuilder) =>
-            {
-                containerBuilder.RegisterModule(autoFacConfigurationModule);
-                //containerBuilder.RegisterType<IpTablesDotNetSystem>().As<IIpTablesSystem>().SingleInstance();
-            }
-            );
-
-            // Add services to the container.
-            builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-            builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
-
+            builder.Host.ConfigureContainer<ContainerBuilder>(startup.ConfigureContainer);
 
             var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
-
-            app.UseAuthorization();
-
-
-            app.MapControllers();
+            startup.Configure(app, app.Environment);
 
             app.Run();
-        }
-
-        private static bool IsLinux
-        {
-            get
-            {
-                int p = (int)Environment.OSVersion.Platform;
-                return (p == 4) || (p == 6) || (p == 128);
-            }
         }
     }
 }
---
+++ Source/BSN.IpTables.Api/Startup.cs
@@ -0,0 +1,62 @@
+﻿using Autofac;
+using Autofac.Configuration;
+
+namespace BSN.IpTables.Api
+{
+    /// <summary>
+    /// Based on https://learn.microsoft.com/en-us/aspnet/core/migration/50-to-60?view=aspnetcore-7.0&tabs=visual-studio#use-startup-with-the-new-minimal-hosting-model
+    /// </summary>
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        //  Using a custom DI container
+        public void ConfigureContainer(ContainerBuilder builder)
+        {
+            var configurationBuilder = new ConfigurationBuilder();
+            configurationBuilder.AddJsonFile("autofac.json");
+            var autoFacConfigurationModule = new ConfigurationModule(configurationBuilder.Build());
+
+            builder.RegisterModule(autoFacConfigurationModule);
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Add services to the container.
+            services.AddControllers();
+
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen();
+        }
+
+        public void Configure<App>(App app, IWebHostEnvironment env) where App : IApplicationBuilder, IEndpointRouteBuilder
+        {
+            // Configure the HTTP request pipeline.
+            if (env.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+        }
+
+        private static bool IsLinux
+        {
+            get
+            {
+                int p = (int)Environment.OSVersion.Platform;
+                return (p == 4) || (p == 6) || (p == 128);
+            }
+        }
+    }
+}
