605241a57d3558c438bf94fae090b5ff47f05e14
soroshsabz <soorosh_abi@hotmail.com>
2023-08-18 12:33:30 +0330

    Add some readme

--- Source/BSN.IpTables.Cli/Generate-PowerShellClient.ps1
+++ Source/BSN.IpTables.Cli/Generate-PowerShellClient.ps1
@@ -6,5 +6,4 @@
 # nvm use 18.16.1
 
 npm install -g "autorest"
-# autorest --powershell --input-file:"../BSN.SipFirewall.Api/swagger.json" --verbose
 autorest --powershell --input-file:"../BSN.IpTables.Api/swagger.json" --verbose
