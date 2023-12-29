ec1cf7702c504155d53f0a218aec8d6180bc1d03
Amirhossein <Amirhossein@DESKTOP-RGEIFHK>
2023-12-30 00:18:33 +0330

    Reuest url modification simplified in module.cs

--- Source/BSN.IpTables.Cli/custom/Module.cs
+++ Source/BSN.IpTables.Cli/custom/Module.cs
@@ -26,32 +26,12 @@ namespace BSN.IpTables.V1
             BSN.IpTables.V1.Runtime.IEventListener callback,
             BSN.IpTables.V1.Runtime.ISendAsync next)
         {
-            string serverAddress = Environment.GetEnvironmentVariable("ServerAddress");
+            string serverAddress = Environment.GetEnvironmentVariable("ServerAddress").ToString();
             string requestUriString = request.RequestUri.ToString();
-
-            // Find the indices of the first and second "/" in the RequestUri
-            int initialBackslashIndex = requestUriString.IndexOf('/');
-            int firstBackslashIndex = requestUriString.IndexOf('/', initialBackslashIndex + 1);
-            int secondBackslashIndex = requestUriString.IndexOf('/', initialBackslashIndex + 2);
-
-            // Check if both backslashes are found
-            if (firstBackslashIndex != -1 && secondBackslashIndex != -1)
-            {
-                // Extract the substring between the first and second "/"
-                string uriSubstring = serverAddress.Trim();
-                string uriFinal = uriSubstring.ToString();
-                string firstString = requestUriString.Substring(0, firstBackslashIndex + 1);
-                string secondString = requestUriString.Substring(secondBackslashIndex);
-                string modifiedUriString = firstString + uriFinal + secondString;
-                // Replace the original substring with the modified substring in the RequestUri
-                request.RequestUri = new Uri(modifiedUriString);
-            }
-            else
-            {
-                // If backslashes are not found, handle the error or take appropriate action
-                throw new NullReferenceException("Backslashes not found in the RequestUri.l!");
-            }
-
+            Uri newUri = new Uri(requestUriString);   
+            string host = newUri.Host; 
+            string finalUrl = requestUriString.Replace(host,serverAddress); 
+            request.RequestUri = new Uri(finalUrl);
             if (next == null)
                 throw new NullReferenceException("Next is null!");
 
