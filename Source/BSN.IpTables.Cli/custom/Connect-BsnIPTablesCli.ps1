328fee86c295fc90b4957d1f2c25f7c722bc9990
Amirhossein <Amirhossein@DESKTOP-RGEIFHK>
2024-01-02 01:54:14 +0330

    Connect method modified for 2023-12-31 comments

--- Source/BSN.IpTables.Cli/custom/Connect-BsnIPTablesCli.ps1
+++ Source/BSN.IpTables.Cli/custom/Connect-BsnIPTablesCli.ps1
@@ -1,3 +1,24 @@
+<#
+.SYNOPSIS
+This cmdlet establishes a connection to the BSN IP Tables with the specified server address.
+
+.DESCRIPTION
+The Connect-BsnIPTablesCli cmdlet connects to the BSN IP Tables using the provided server address. 
+It is a mandatory parameter, and the connection is established in the begin block.
+
+.PARAMETER ServerAddress
+Specifies the target server address for the connection. This is a mandatory parameter.
+
+.EXAMPLE
+Connect-BsnIPTablesCli -ServerAddress "http://example.com"
+Establishes a connection to the BSN IP Tables with the server address "http://example.com".
+
+.NOTES
+File Name      : Connect-BsnIPTablesCli.ps1
+Prerequisite   : PowerShell V5
+Copyright 2019 - The BSN Team
+#>
+
 function Connect-BsnIPTablesCli {
     [CmdletBinding()]
     param(
@@ -9,6 +30,11 @@ function Connect-BsnIPTablesCli {
     )
 
     begin {
+        # Check if $ServerAddress is null
+        if ($null -eq $ServerAddress) {
+            Write-Error "ServerAddress is mandatory. Please provide a valid value."
+            return
+        }
         # Save the ServerAddress in a session variable
         $env:ServerAddress = $ServerAddress
     }
--- Source/BSN.IpTables.Cli/custom/Module.cs
+++ Source/BSN.IpTables.Cli/custom/Module.cs
@@ -1,3 +1,9 @@
+//The partial class Module appears to be a part of an application that utilizes AutoRest-generated code. AutoRest is a tool used for generating client libraries for accessing RESTful web services. In this context, the Module partial class is likely used to extend or modify the behavior of the AutoRest-generated classes and methods.
+//Here we are trying to get enviromental variable mean server address and set to requst of all urls in SendAsync method
+//Also we have AfterCreatePipeline,BeforeCreatePipeline and CustomInit that are called at the required places to do somethings
+//Pipeline Modification: The Module class contains methods (AfterCreatePipeline and BeforeCreatePipeline) that seem to be involved in the creation of an HTTP pipeline (HttpPipeline). This pipeline is likely used for handling HTTP requests and responses.
+//SendAsync Method: The SendAsync method is asynchronous and is involved in processing HTTP requests. It uses the GetIptabaleServerAddressAsync method to obtain a server address, modifies the request URI accordingly, and then delegates to the next step in the pipeline.
+
 using System;
 using System.IO;
 using System.Net.Http;
@@ -8,29 +14,36 @@ namespace BSN.IpTables.V1
 {
     public partial class Module
     {
-        partial void AfterCreatePipeline(global::System.Management.Automation.InvocationInfo invocationInfo,
-            ref BSN.IpTables.V1.Runtime.HttpPipeline pipeline)
+        partial void AfterCreatePipeline(
+            global::System.Management.Automation.InvocationInfo invocationInfo,
+            ref BSN.IpTables.V1.Runtime.HttpPipeline pipeline
+        )
         {
             if (pipeline == null)
                 throw new NullReferenceException("Pipeline is null!");
             pipeline.Append(SendAsync);
         }
 
-        partial void BeforeCreatePipeline(global::System.Management.Automation.InvocationInfo invocationInfo,
-            ref BSN.IpTables.V1.Runtime.HttpPipeline pipeline)
-        {
-        }
+        partial void BeforeCreatePipeline(
+            global::System.Management.Automation.InvocationInfo invocationInfo,
+            ref BSN.IpTables.V1.Runtime.HttpPipeline pipeline
+        ) { }
 
-        public async System.Threading.Tasks.Task<System.Net.Http.HttpResponseMessage>
-           SendAsync(System.Net.Http.HttpRequestMessage request,
+        public async System.Threading.Tasks.Task<System.Net.Http.HttpResponseMessage> SendAsync(
+            System.Net.Http.HttpRequestMessage request,
             BSN.IpTables.V1.Runtime.IEventListener callback,
-            BSN.IpTables.V1.Runtime.ISendAsync next)
+            BSN.IpTables.V1.Runtime.ISendAsync next
+        )
         {
             string serverAddress = Environment.GetEnvironmentVariable("ServerAddress").ToString();
+            if (serverAddress == null)
+            {
+                Console.WriteLine("ServerAddress variable is not set.");
+            }
             string requestUriString = request.RequestUri.ToString();
-            Uri newUri = new Uri(requestUriString);   
-            string host = newUri.Host; 
-            string finalUrl = requestUriString.Replace(host,serverAddress); 
+            Uri newUri = new Uri(requestUriString);
+            string host = newUri.Host;
+            string finalUrl = requestUriString.Replace(host, serverAddress);
             request.RequestUri = new Uri(finalUrl);
             if (next == null)
                 throw new NullReferenceException("Next is null!");
@@ -38,8 +51,6 @@ namespace BSN.IpTables.V1
             return await next.SendAsync(request, callback);
         }
 
-        partial void CustomInit()
-        {
-        }
+        partial void CustomInit() { }
     }
 }
